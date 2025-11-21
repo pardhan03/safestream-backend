6005c196c17fd10da5f65f5b51ea7bac221bdb58
Manish Pardhan <pardhanmanish7887@gmail.com>
2025-11-21 12:07:01 +0530

    authorization fix

--- middlewares/secureRoute.js
+++ middlewares/secureRoute.js
@@ -3,7 +3,8 @@ import { User } from "../models/user.modal.js";
 
 const secureRoute = async (req, res, next) => {
   try {
-    const token = req.cookies.token;
+    // const token = req.cookies.token;
+    const token = req.cookies.token || req.headers.authorization?.split(" ")[1];
     if (!token) {
       return res.status(401).json({ error: "No token, authorization denied" });
     }
