8b5a8883408c86f1b78b463722350678c09ebbcb
Manish Pardhan <pardhanmanish7887@gmail.com>
2025-11-21 11:11:59 +0530

    fix cors error

--- index.js
+++ index.js
@@ -15,12 +15,25 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 
+const allowedOrigins = [
+  'http://localhost:5173',
+  'https://serene-kleicha-ff250b.netlify.app'
+];
+
 const corsOptions = {
-    origin: 'http://localhost:5173',
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization']
+  origin: function(origin, callback) {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
 };
+
 app.use(cors(corsOptions));
 
 // Database
