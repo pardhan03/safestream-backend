8c49a61d2402a36485e7e78104794fcb036925b6
Manish Pardhan <pardhanmanish7887@gmail.com>
2025-11-21 10:32:40 +0530

    added mongodb atlas cofig

--- database/database.js
+++ database/database.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const databaseConnection = () => {
-    mongoose.connect('mongodb+srv://manish_db_user:TDW3LlgEoD5kTTUZ@cluster0.fhw6wrg.mongodb.net/safestream').then(()=>{
+    mongoose.connect('mongodb+srv://manish_db_user:TDW3LlgEoD5kTTUZ@cluster0.fhw6wrg.mongodb.net/myDatabase?appName=Cluster0').then(()=>{
         console.log("Connected to mongoDB");
     }).catch((error)=>{
         console.log(error);
